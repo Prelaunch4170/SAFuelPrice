170fc919e3e5c3d41682c8dbae2be0236aadccf2
Andre <44639490+INeedHelp01@users.noreply.github.com>
2023-09-26 15:20:35 +0930

    Added my servo

--- python/CallApi.py
+++ python/CallApi.py
@@ -22,7 +22,7 @@ def Call_api():
     siteJSON = responseSite.json()
 
     # Replace this with the SiteId you want to filter by
-    desiredSiteId = [11111]
+    desiredSiteId = [61577374]
 
     # Filter out the fuel prices for the desired SiteId
     fuelPriceString = getSitePrice(siteJSON, fuelJSON, desiredSiteId)
